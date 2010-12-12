fd2779ee31bdbdaffa8d62329e1e6042b68fbd49
tunnr <tunnerkrisztian@freemail.hu>
2010-12-12 12:18:49 +0100

    Fix bugs(issue 1 closed)

---
+++ updates/182_world_creature_template.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_template` SET `flags` = '4098' WHERE `entry` =15351;
+UPDATE `creature_template` SET `flags` = '4098' WHERE `entry` =15350;
---
+++ updates/182_world_gameobject_template.sql
@@ -0,0 +1,3 @@
+UPDATE `gameobject_template` SET `data1` = '900' WHERE `gameobject_template`.`entry` =4072;
+UPDATE `gameobject_template` SET `data1` = '900' WHERE `gameobject_template`.`entry` =61936;
+UPDATE `gameobject_template` SET `data1` = '900' WHERE `gameobject_template`.`entry` =61935;
